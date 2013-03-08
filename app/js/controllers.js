138e033177bd556269ecdf88791c7197f4e97916
Tamerlan-i7 <trybynenko@gmail.com>
2013-03-08 02:46:27 +0200

    second level

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -7,17 +7,18 @@ angular.module('menuApp', ['menuAppFilters']);
 /* Controllers */
 function MenuCtrl($scope,$location,$window) {
     //Construct:
-    var active_menu;
+    $scope.active_menu=false;
     $scope.menus = menus;
+
     angular.forEach($scope.menus, function(value, key){
         if($scope.menus[key].url == $window.location.pathname)
-            (active_menu = $scope.menus[key]).active=1;
+            ($scope.active_menu = $scope.menus[key]).active=1;
     });
 
     //Scope function:
     $scope.active = function() {
-        active_menu.active = 0;
-        (active_menu = $scope.menus[this.$index]).active = 1;
+        $scope.active_menu.active = 0;
+        ($scope.active_menu = $scope.menus[this.$index]).active = 1;
     };
 }  MenuCtrl.$inject = ['$scope','$location','$window'];
 
--- app/js/resources.js
+++ app/js/resources.js
@@ -6,30 +6,46 @@
  */
 
 var menus =[
-    {"name": "User",
+    {
+        "name": "User",
         "title":"user",
         "url":'/ng-test-prj/app/index.html',
         "submenu": [{
+            "name": "Add User",
             "title":"Add user",
             "url":'#'
         },{
+            "name": "Edit User",
             "title":"Edit user",
             "url":'#'
         }]
     },
-    {"name": "Video",
+    {
+        "name": "Video",
         "title":"video",
         "url":'#'
         },
-    {"name": "Player",
+    {
+        "name": "Player",
         "title":"player",
-        "url":'/ng-test-prj/app/'
+        "url":'/ng-test-prj/app/',
+        "submenu": [{
+            "name": "Add Player",
+            "title":"Add Player",
+            "url":'#'
+        },{
+            "name": "Edit Player",
+            "title":"Edit Player",
+            "url":'#'
+        }]
     },
-    {"name": "Playlist",
+    {
+        "name": "Playlist",
         "title":"Playlist",
         "url":'#'
     },
-    {"name": "User-Account",
+    {
+        "name": "User-Account",
         "title":"user-account",
         "url":'#'
 //    , "active":1
