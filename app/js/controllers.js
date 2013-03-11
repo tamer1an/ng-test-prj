cca1e1d8a41650a85afbb0e12984b8cb6588048a
Tamerlan-i7 <trybynenko@gmail.com>
2013-03-11 02:25:22 +0200

    reformat json lvl works correct

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -5,46 +5,57 @@ angular.module('menuApp', ['menuAppFilters']);
 
 /* Controllers */
 function MenuCtrl($scope,$location,$window) { //console.log($window.location.pathname);
-// Construct
-  $scope.menuApp = {"lvl_1" : {"src":[],"active":false},"maxRenderLvl":1};
+  // Construct
+
+  $scope.menuApp = {"src":[],"active":false,"maxRenderLvl":1,"itemLvl":0};
 
     angular.forEach(dataSrc,function(v,k){
-      var lvlCounter = 1;
+      var lvlCounter = 0;
+      v.itemLvl = lvlCounter;
+      v.itemIdx = k;
 
-     if(v.url == $window.location.pathname)
-        $scope.menuApp.lvl_1.active = k;
+//     if(v.url == $window.location.pathname)
+//        $scope.menuApp.lvl_1.active = k;
 
-      if(typeof v.submenu=='object')
-   
+      if(typeof v.submenu=='object'){
         this.push(
          (function getSubLvl(v,lvlCounter,parentIdx){
+            var tmpContainer = false;
             lvlCounter++;
 
-            v['lvl_'+parentIdx+'_'+lvlCounter] = {};
-            v['lvl_'+parentIdx+'_'+lvlCounter].src = v.submenu;
-            v['lvl_'+parentIdx+'_'+lvlCounter].active = false;
-
             for(var idx in v.submenu){
-                if(v.submenu[idx].url == $window.location.pathname)
-                    v['lvl_'+parentIdx+'_'+lvlCounter].active = idx;
+//              if(v.submenu[idx].url == $window.location.pathname)
+//                 v['lvl_'+lvlCounter+'_'+parentIdx].active = idx;
 
-                //console.log('idx -'+idx, v.submenu[idx])
-                if(typeof v.submenu[idx]=='object')
-                    v['lvl_'+parentIdx+'_'+lvlCounter].src.push(getSubLvl(v.submenu[idx],lvlCounter,idx));
+                v.submenu[idx].itemLvl = lvlCounter;
+                v.submenu[idx].itemIdx = parseInt(idx);
 
-                //console.log(lvlCounter)
-            }
+                if(typeof v.submenu[idx].submenu=='object'){
+                    lvlCounter++;
+
+                    tmpContainer = v.submenu[idx].submenu;
+                    v.submenu[idx].submenu = [];
+
+                    for(var index in tmpContainer){
+                        tmpContainer[index].itemLvl=lvlCounter;
+                        tmpContainer[index].itemIdx=parseInt(index);
+                        v.submenu[idx].submenu.push(getSubLvl(tmpContainer[index],lvlCounter,index));
+                    }
+                    tmpContainer = false;
+                }
+            } // for in submenu
             return v;
-          })(v,lvlCounter,k)
-        );
 
-      else this.push(v);
+          })(v,lvlCounter,k) // getSubLvl
+        );  // push
 
-    },$scope.menuApp.lvl_1.src) 
-    
-   console.info($scope.menuApp);
+      } else {
+          this.push(v);
+      }
+
+    },$scope.menuApp.src) //ngForEach
+    console.info($scope.menuApp);
 }  MenuCtrl.$inject = ['$scope','$location','$window'];
-
 
 /* Filters */
 angular.module('menuAppFilters', []).filter('active', function() {
