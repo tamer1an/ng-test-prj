b5f171df7d491fd39ab92f366206b8d19f43e07e
Tamerlan-i7 <trybynenko@gmail.com>
2013-03-23 00:04:16 +0200

    partial system begins

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,8 +4,6 @@
 
 
 function MyCtrl1($scope) {
-
-	
 	var scene, camera, render, container, controls;
 	var W,H;
 	
@@ -36,9 +34,6 @@ function MyCtrl1($scope) {
         earth.position.x = 1500;
 
     scene.add(earth);
-
-
-
 	////////////////////////////
 	
 	
@@ -83,7 +78,77 @@ function MyCtrl1($scope) {
 MyCtrl1.$inject = ['$scope'];
 
 
-function MyCtrl2() {}
+function MyCtrl2() {
+    var scene, camera, render, container, controls;
+    var W,H;
+
+    // var light;	// var info = false;	// var infoMercury = false;
+
+    W = parseInt(document.body.clientWidth);
+    H = parseInt(document.body.clientHeight);
+
+    container = document.createElement('div');
+    document.body.appendChild(container);
+
+    camera = new THREE.PerspectiveCamera(45,W/H,1,10000);
+    camera.position.z = 6300;
+    scene = new THREE.Scene();
+
+
+    //sun
+    var sun_geom = new THREE.SphereGeometry(430,30,30),
+        sun_mat = new THREE.MeshNormalMaterial(),
+        sun = new THREE.Mesh(sun_geom,sun_mat);
+
+    scene.add(sun);
+
+    //earth
+    var earth_geom = new THREE.SphereGeometry(100,30,30),
+        earth_mat = new THREE.MeshNormalMaterial(),
+        earth = new THREE.Mesh(earth_geom,earth_mat);
+    earth.position.x = 1500;
+
+    scene.add(earth);
+    ////////////////////////////
+
+
+
+    ////////////////////////////
+    render = new THREE.WebGLRenderer({antialias: true});
+    render.setSize(W,H);
+    container.appendChild(render.domElement); // console.log(container) // console.log(scene)
+
+    var t=0,y=0;
+
+    document.addEventListener('mousemove',function(event){
+        y = parseInt(event.offsetY);
+    });
+
+    animate();
+
+    function animate(){
+        requestAnimationFrame(animate);
+        sun.rotation.y+=0.002;
+
+        earth.rotation.y+=0.002;
+        earth.position.x = Math.sin(t*0.2)*2000;
+        earth.position.z = Math.cos(t*0.2)*1700;
+
+// camera angle-view experiments
+//   camera.lookAt(earth.position);
+
+//   camera.position.x = earth.position.x;
+//   camera.position.z = earth.position.z;
+//   camera.lookAt(sun.position);
+
+        camera.position.y = y*5;
+        camera.lookAt(scene.position);
+
+        t+=Math.PI/180*2;
+
+        render.render(scene,camera);
+    }
+}
 MyCtrl2.$inject = [];
 
 
