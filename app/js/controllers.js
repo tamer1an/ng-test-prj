09784c4871b6a6d1c069246870bb4ea5408cc6ab
clipkit-git <trybynenko@gmail.com>
2013-03-22 14:52:34 +0200

    sun primitive added

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -7,9 +7,8 @@ function MyCtrl1($scope) {
 	
 	var scene, camera, render, container, controls;
 	var W,H;
-	var light;
-	var info = false;
-	var infoMercury = false;
+	
+	// var light;	// var info = false;	// var infoMercury = false;
 	
 	W = parseInt(document.body.clientWidth);
 	H = parseInt(document.body.clientHeight);
@@ -17,18 +16,36 @@ function MyCtrl1($scope) {
 	container = document.createElement('div');
 	document.body.appendChild(container);
 
-	camera = new THREE.PerspectiveCamera(45,W/H,1,1000000);
-	camera.position.z = 20000;
+	camera = new THREE.PerspectiveCamera(45,W/H,1,10000);
+	camera.position.z = 4300;
 	scene = new THREE.Scene();
 	
 	
+	//sun
+	var sun, sun_geom,sun_mat;
+
+	sun_geom = new THREE.SphereGeometry(430,30,30);
+    sun_mat = new THREE.MeshNormalMaterial();
+    sun = new THREE.Mesh(sun_geom,sun_mat);
+
+    scene.add(sun);
+	////////////////////////////
 	
+	
+	
+	////////////////////////////
 	render = new THREE.WebGLRenderer({antialias: true});
 	render.setSize(W,H);
 	container.appendChild(render.domElement);
-	
+    animate();
+
 	// console.log(container)
 	// console.log(scene)
+
+    function animate(){
+        requestAnimationFrame(animate);
+        render.render(scene,camera);
+    }
 }
 
 MyCtrl1.$inject = [];
