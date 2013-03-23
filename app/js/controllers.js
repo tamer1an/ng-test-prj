bf2f1bd380b9b68a3c8de0bed0f30a3541e3530a
Tamerlan-i7 <trybynenko@gmail.com>
2013-03-23 03:41:54 +0200

    Particles geometry rendered

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -80,9 +80,7 @@ MyCtrl1.$inject = ['$scope'];
 
 function MyCtrl2() {
     var scene, camera, render, container, controls;
-    var W,H;
-
-    // var light;	// var info = false;	// var infoMercury = false;
+    var W,H;  // var light;	// var info = false;	// var infoMercury = false;
 
     W = parseInt(document.body.clientWidth);
     H = parseInt(document.body.clientHeight);
@@ -90,62 +88,36 @@ function MyCtrl2() {
     container = document.createElement('div');
     document.body.appendChild(container);
 
-    camera = new THREE.PerspectiveCamera(45,W/H,1,10000);
-    camera.position.z = 6300;
+    camera = new THREE.PerspectiveCamera(45,W/H,1,1000000);
+    camera.position.z = 1;
     scene = new THREE.Scene();
 
+    var starsGeometry = new THREE.Geometry();
+    var starsMaterial = new THREE.ParticleBasicMaterial({size:1,sizeAttenuation:false});
+    var stars; //real partice object
 
-    //sun
-    var sun_geom = new THREE.SphereGeometry(430,30,30),
-        sun_mat = new THREE.MeshNormalMaterial(),
-        sun = new THREE.Mesh(sun_geom,sun_mat);
+    for(var i=0;i<5000;i++){
+        var vertex = new THREE.Vector3();
+        vertex.x = Math.random()*2-1;
+        vertex.y = Math.random()*2-1;
+        vertex.z = Math.random()*2-1;
+//        vertex.multiplyScalar(6000);
+        starsGeometry.vertices.push(vertex);
+    }
 
-    scene.add(sun);
-
-    //earth
-    var earth_geom = new THREE.SphereGeometry(100,30,30),
-        earth_mat = new THREE.MeshNormalMaterial(),
-        earth = new THREE.Mesh(earth_geom,earth_mat);
-    earth.position.x = 1500;
-
-    scene.add(earth);
-    ////////////////////////////
-
-
+    stars = new THREE.ParticleSystem(starsGeometry,starsMaterial);
+    scene.add(stars);
 
     ////////////////////////////
     render = new THREE.WebGLRenderer({antialias: true});
     render.setSize(W,H);
     container.appendChild(render.domElement); // console.log(container) // console.log(scene)
 
-    var t=0,y=0;
-
-    document.addEventListener('mousemove',function(event){
-        y = parseInt(event.offsetY);
-    });
 
     animate();
 
     function animate(){
         requestAnimationFrame(animate);
-        sun.rotation.y+=0.002;
-
-        earth.rotation.y+=0.002;
-        earth.position.x = Math.sin(t*0.2)*2000;
-        earth.position.z = Math.cos(t*0.2)*1700;
-
-// camera angle-view experiments
-//   camera.lookAt(earth.position);
-
-//   camera.position.x = earth.position.x;
-//   camera.position.z = earth.position.z;
-//   camera.lookAt(sun.position);
-
-        camera.position.y = y*5;
-        camera.lookAt(scene.position);
-
-        t+=Math.PI/180*2;
-
         render.render(scene,camera);
     }
 }
