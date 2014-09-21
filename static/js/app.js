864cef6e3c5aadb89520f07b5803b551bbb803e4
Tamerlan-i7 <trybynenko@gmail.com>
2014-09-22 02:02:46 +0300

    routin added

--- static/js/app.js
+++ static/js/app.js
@@ -10,7 +10,7 @@
  */
 
 var INApp = {
-    app : angular.module('adminCRUD', [  ])
+    app : angular.module('adminCRUD', [ 'ngRoute' ])
 };
 
 /**
@@ -31,3 +31,10 @@ angular.module('adminCRUD').directive('users', function() {
 });
 
 
+// Declare app level module which depends on filters, and services  ['myApp.filters', 'myApp.services', 'myApp.directives']
+angular.module('adminCRUD').
+  config(['$routeProvider', function($routeProvider) {
+    $routeProvider.when('/view1', {templateUrl: '../partials/partial1.html', controller: 'usersCtrl'});
+    // $routeProvider.when('/view2', {templateUrl: '../partials/partial2.html', controller: MyCtrl2});
+    $routeProvider.otherwise({redirectTo: '/view1'});
+  }]);
