8557f1e5942a9479274a2b85742386f75c852378
Tamerlan <tamerjlah@gmail.com>
2015-03-29 11:50:20 +0000

    cean branch

--- app/js/filters.js
+++
@@ -1,8 +0,0 @@
-//'use strict';
-//
-///* Filters */
-//angular.module('menuAppFilters', []).filter('active', function() {
-//    return function(input) {
-//        return input ? 'active' : '';
-//    };
-//});
